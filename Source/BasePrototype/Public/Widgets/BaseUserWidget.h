c4f7188b07d41130aef667aa0ce7541f4b6e6090
DreadDev1 <79171586+DreadDev1@users.noreply.github.com>
2025-07-24 19:12:40 -0400

    Add BaseWidgetContoller and BaseUserWidget

---
+++ Source/BasePrototype/Public/Widgets/BaseUserWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BaseUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASEPROTOTYPE_API UBaseUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
